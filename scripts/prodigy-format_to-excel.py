aa83d5f5d1ddc7ddb5a11f7c4d584af9b08316bf
Kristian Gade Kjelmann <kgk@adm.aau.dk>
2022-04-29 08:53:00 +0200

    tidy data option

--- scripts/prodigy-format_to-excel.py
+++ scripts/prodigy-format_to-excel.py
@@ -11,8 +11,10 @@ data_in = 'tpb_tweets_simple-filter_labelled-prodigy-format_20220408.json'
 data_out_n = 'tpb_tweets_labelled.xlsx'
 
 # columns names for labels
-#label_cols = ["Physical stuckness and Covid","Pandemic precarity","Blocked and derailed mobilities because of Covid","New mobilities in relation to Covid","Other"]
-label_cols = ['blocked mobilities', 'new mobilities', 'pandemic precarity','physical stuckness', 'surrounding precarity and vulnerabilities']
+old_label_cols = ['Physical stuckness and Covid', 'Pandemic precarity', 'Blocked and derailed mobilities because of Covid', 'New mobilities in relation to Covid', 'Other']
+new_label_cols = ['Physical stuckness and Covid', 'Pandemic precarity', 'Blocked and derailed mobilities in relation to Covid', 'Mobility in relation to Covid ', 'Other']
+
+label_rename_map = dict(zip(old_label_cols, new_label_cols)
 
 data_d = os.path.join('..', 'data')
 
@@ -32,15 +34,22 @@ with open(data_in_p, 'r') as f:
 
 df_in = pd.DataFrame.from_records(data).drop_duplicates(subset = 'id')
 
-# convert labels to dummy variables
+# tidy - one row per label
 df_in = df_in.explode('accept')
-df_in['accepted'] = 1
-df_in_labeldummies = pd.merge(df_raw, df_in.pivot(index = 'id', columns="accept", values="accepted").reset_index().loc[:, ['id'] + label_cols].fillna(0), how = 'inner', on = 'id')
+df_in['accept'] = df_in['accept'].replace(label_rename_map)
+df_merged = pd.merge(df_in.loc[:, 'id', 'accept']], df_raw, how = 'inner', on = 'id')
 
-# reorder columns
-columns_neworder = ['created_at', 'name', 'username', 'text', 'tweet_link', 'covid'] + label_cols + ['id', 'hashtags', 'urls', 'retweet_count', 'reply_count',
-                                                                                                     'like_count', 'quote_count', 'referenced_type', 'referenced_id']
-df_out = df_in_labeldummies.loc[:, columns_neworder]
+# convert labels to dummy variables
+#df_in['accepted'] = 1
+#df_in_labeldummies = pd.merge(df_raw, df_in.pivot(index = 'id', columns="accept", values="accepted").reset_index().loc[:, ['id'] + label_cols].fillna(0), how = 'inner', on = 'id')
+
+# reorder columns - dummies
+#columns_neworder = ['created_at', 'name', 'username', 'text', 'tweet_link', 'covid'] + label_cols + ['id', 'hashtags', 'urls', 'retweet_count', 'reply_count', 'like_count', 'quote_count', 'referenced_type', 'referenced_id']
+#df_out = df_in_labeldummies.loc[:, columns_neworder]
+
+# reorder columns - tidy
+columns_neworder = ['created_at', 'name', 'username', 'text', 'tweet_link', 'covid', 'accept', 'id', 'hashtags', 'urls', 'retweet_count', 'reply_count', 'like_count', 'quote_count', 'referenced_type', 'referenced_id']
+df_out = df_merged.loc[:, columns_neworder].rename(columns = {'accept': 'label'})
 
 # export
 df_out.to_excel(data_out_p, index = False)
